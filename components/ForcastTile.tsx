f32b9147cf5083e5ddcb08740c65218665d9ae9b
ceejayps <ceejayps1308@gmail.com>
2022-11-29 22:05:48 -0500

    Create ForcastTile.tsx

---
+++ components/ForcastTile.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type Props = {context: object}
+
+function ForcastTile({}: Props) {
+  return (
+    <div className='h-[200px] w-[170px] bg-white flex-col flex justify-evenly items-center rounded-md' >
+        <h3>Today</h3>
+        <  img src="http://openweathermap.org/img/wn/10d@2x.png
+
+" alt="" className='h-[50px] w-[50px]'/>
+        <p>Humidity</p>
+        <h3>30%</h3>
+    </div>
+  )
+}
+
+export default ForcastTile
