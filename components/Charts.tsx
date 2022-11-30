398a044181bf22f9de74e36156a7578056a66d39
ceejayps <ceejayps1308@gmail.com>
2022-11-29 22:05:10 -0500

    Create Charts.tsx

---
+++ components/Charts.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import DaileyForcast from './DaileyForcast'
+import SplineGraph from './SplineGraph'
+
+type Props = {
+    context:Apiresponse};
+
+    type Apiresponse = {
+        list: ResponseList[];
+      };
+      type ResponseList =  {
+        "dt": number,
+        "main": {
+            "temp": number,
+            "feels_like": number,
+            "temp_min": number,
+            "temp_max": number,
+            "pressure": number,
+            "sea_level": number,
+            "grnd_level": number,
+            "humidity": number,
+            "temp_kf": number
+        },
+        "weather": [
+            {
+                "id": number,
+                "main": string,
+                "description": string,
+                "icon": string
+            }
+        ],
+        "clouds": {
+            "all": number
+        },
+        "wind": {
+            "speed": number,
+            "deg": number,
+            "gust": number
+        },
+        "visibility": number,
+        "pop":number,
+        "sys": object,
+        "dt_txt": string
+    }
+
+   
+
+const Charts = ({context}: Props) => {
+  return (
+    <div className='lg:flex-[6_6_0]  w-full h-[60vh] lg:h-screen lg:h-full bg-slate-900 justify-center items-center lg:flex flex-col  p-5  lg:p-20 lg:rounded-r-xl space-y-10 '>
+        <SplineGraph context={context}/>
+        <DaileyForcast context={context}/>
+    </div>
+  )
+}
+
+export default Charts
