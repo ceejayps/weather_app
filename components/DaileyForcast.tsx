2c582666806632862c0e178b501215e1b5f2a394
ceejayps <ceejayps1308@gmail.com>
2022-11-30 11:38:03 -0500

    Update DaileyForcast.tsx

--- components/DaileyForcast.tsx
+++ components/DaileyForcast.tsx
@@ -3,16 +3,54 @@ import React from 'react'
 import ForcastTile from './ForcastTile'
 
 type Props = {
-    context: object
+    context: Apiresponse
+}
+
+type Apiresponse = {
+    list: ResponseList[];
+  };
+  type ResponseList =  {
+    "dt": number,
+    "main": {
+        "temp": number,
+        "feels_like": number,
+        "temp_min": number,
+        "temp_max": number,
+        "pressure": number,
+        "sea_level": number,
+        "grnd_level": number,
+        "humidity": number,
+        "temp_kf": number
+    },
+    "weather": [
+        {
+            "id": number,
+            "main": string,
+            "description": string,
+            "icon": string
+        }
+    ],
+    "clouds": {
+        "all": number
+    },
+    "wind": {
+        "speed": number,
+        "deg": number,
+        "gust": number
+    },
+    "visibility": number,
+    "pop":number,
+    "sys": object,
+    "dt_txt": string
 }
 
 const DaileyForcast = ({context}: Props) => {
   return (
     <div className=' w-full h-[200px] flex justify-evenly space-x-1 mt-20'>
         <ForcastTile context={context} date={moment().add(1,'days')}/>
-        <ForcastTile context={context} date={moment().add(1,'days')}/>
-        <ForcastTile context={context} date={moment().add(1,'days')}/>
-        <ForcastTile context={context} date={moment().add(1,'days')}/>
+        <ForcastTile context={context} date={moment().add(2,'days')}/>
+        <ForcastTile context={context} date={moment().add(3,'days')}/>
+        <ForcastTile context={context} date={moment().add(4,'days')}/>
     </div>
   )
 }
