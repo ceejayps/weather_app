a3f8bc77a1287663e8576a854257175b88f68dd2
ceejayps <ceejayps1308@gmail.com>
2022-11-30 06:11:39 -0500

    Update DaileyForcast.tsx

--- components/DaileyForcast.tsx
+++ components/DaileyForcast.tsx
@@ -1,3 +1,4 @@
+import moment from 'moment'
 import React from 'react'
 import ForcastTile from './ForcastTile'
 
@@ -8,10 +9,10 @@ type Props = {
 const DaileyForcast = ({context}: Props) => {
   return (
     <div className=' w-full h-[200px] flex justify-evenly space-x-1 mt-20'>
-        <ForcastTile context={context}/>
-        <ForcastTile context={context}/>
-        <ForcastTile context={context}/>
-        <ForcastTile context={context}/>
+        <ForcastTile context={context} date={moment().add(1,'days')}/>
+        <ForcastTile context={context} date={moment().add(1,'days')}/>
+        <ForcastTile context={context} date={moment().add(1,'days')}/>
+        <ForcastTile context={context} date={moment().add(1,'days')}/>
     </div>
   )
 }
