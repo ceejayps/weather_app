17bbdc559517c4b7f1c32af3c2c88ad43994a285
ceejayps <ceejayps1308@gmail.com>
2022-11-30 11:38:06 -0500

    Update ForcastTile.tsx

--- components/ForcastTile.tsx
+++ components/ForcastTile.tsx
@@ -1,17 +1,65 @@
+import moment from 'moment';
 import { Moment } from 'moment'
 import React from 'react'
 
-type Props = {context: object, date:Moment}
+type Props = {context: Apiresponse, date:Moment}
 
-function ForcastTile({}: Props) {
+
+
+type Apiresponse = {
+    list: ResponseList[];
+  };
+  type ResponseList =  {
+    "dt": number,
+    "main": {
+        "temp": number,
+        "feels_like": number,
+        "temp_min": number,
+        "temp_max": number,
+        "pressure": number,
+        "sea_level": number,
+        "grnd_level": number,
+        "humidity": number,
+        "temp_kf": number
+    },
+    "weather": [
+        {
+            "id": number,
+            "main": string,
+            "description": string,
+            "icon": string
+        }
+    ],
+    "clouds": {
+        "all": number
+    },
+    "wind": {
+        "speed": number,
+        "deg": number,
+        "gust": number
+    },
+    "visibility": number,
+    "pop":number,
+    "sys": object,
+    "dt_txt": string
+}
+
+function ForcastTile({date, context}: Props) {
+    const result = context;
+    const consition = result.list.filter((obj)=>moment(obj.dt_txt).isSame(date,'day'))
+    
+    const days =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+    const index = moment().isSame(date,'day')? 0:4
+    //console.log(consition[index]?.dt_txt, ' ',days[date.day()], index , consition[index]?.weather[0].icon)
+    console.log( moment().isSame(date,'day'),days[date.day()],)
   return (
     <div className='h-[200px] w-[170px] bg-white flex-col flex justify-evenly items-center rounded-md' >
-        <h3>Today</h3>
-        <  img src="http://openweathermap.org/img/wn/10d@2x.png
-
-" alt="" className='h-[50px] w-[50px]'/>
+        <h3>{days[date.day()]}</h3>
+        <  img src={`http://openweathermap.org/img/wn/${consition[index]?.weather[0].icon}@2x.png`}
+         alt="" className='h-[50px] w-[50px]'/>
         <p>Humidity</p>
-        <h3>30%</h3>
+        <h3>{consition[0]?.main?.humidity}%</h3>
     </div>
   )
 }
